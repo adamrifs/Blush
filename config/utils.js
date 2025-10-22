13eff142f3ccf3d308f159adae59694fb4ec0139
Adam <112921206+adamrifs@users.noreply.github.com>
2025-10-22 21:33:57 +0530

    first commit

---
+++ config/utils.js
@@ -0,0 +1,14 @@
+const jwt = require('jsonwebtoken')
+const dotenv = require('dotenv')
+dotenv.config()
+
+const generateToken = (id, res) => {
+    const token = jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '5d' })
+    res.cookie("jwt", token, {
+        maxAge: 24 * 60 * 60 * 1000,
+        // httpOnly: true,
+        sameSite: true
+    })
+    return token
+}
+module.exports = generateToken
