be4654ac3a78728277bb5eb64b2feb0e6c5b86e7
Adam <112921206+adamrifs@users.noreply.github.com>
2025-11-29 19:22:20 +0530

    customer update

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -1,9 +1,25 @@
 const User = require("../models/userSchema");
-
+const Order = require('../models/orderSchema')
 exports.getCustomers = async (req, res) => {
     try {
         const users = await User.find().sort({ createdAt: -1 });
-        res.json({ customers: users });
+
+        const customersWithOrders = await Promise.all(
+            users.map(async (u) => {
+                const ordersCount = await Order.countDocuments({ userId: u._id });
+
+                return {
+                    _id: u._id,
+                    name: u.name,
+                    email: u.email,
+                    phone: u.phone || "—",
+                    createdAt: u.createdAt,
+                    totalOrders: ordersCount,
+                };
+            })
+        );
+
+        res.json({ customers: customersWithOrders });
     } catch (err) {
         console.error(err);
         res.status(500).json({ message: "Server error" });
